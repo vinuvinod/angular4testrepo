719d5add7e2f874cf9e3410f31ee02e29b09a55c
Vinod Rajendran <vinod.rajendran@SPERIDIAN.DC>
2017-10-27 12:15:25 +0530

    Just a test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroComponent } from "./heroes.component";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+
 
 @NgModule({
-  declarations: [AppComponent, HeroDetailComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])],
+  declarations: [AppComponent, HeroDetailComponent, HeroComponent, DashboardComponent],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
